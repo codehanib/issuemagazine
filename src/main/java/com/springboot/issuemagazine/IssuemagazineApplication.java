84f7508e65ad9148a0acd002d3fb831f63fd7b04
HHJ <hhyejin122@gmail.com>
2026-08-12 10:25:55 +0900

    8/12 10AM issuemagazine 초기 파일 (시큐리티 전)

---
+++ src/main/java/com/springboot/issuemagazine/IssuemagazineApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.issuemagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuemagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuemagazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/issuemagazine/IssuemagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.issuemagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuemagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
